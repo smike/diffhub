09f38e0198695b575eeba28a4439e10fa6cbc0d6
smike <smike@alum.mit.edu>
2013-03-19 03:20:57 -0400

    creating single table for side-by-side diffs, polish

--- content.js
+++ content.js
@@ -36,10 +36,71 @@ $('#hide-side-diffs').bind('click', function() {
   $('#show-side-diffs').each(show);
 }).each(hide);
 
+function toEnsp() {
+  $(this).html($(this).html().replace(/&nbsp;/g, '&ensp;'));
+}
 
 $(".diff-table").each(function() {
-  var left = $(this).clone();//.addClass('side-left');
-  var right = $(this).clone();//.addClass('side-right');
+  var copy = $(this).clone();
+  $(this).addClass('unified-diff');
+  var new_table = $('<table class="side-by-side-diff diff-table line-number-attrs"></table>');
+  $(new_table).each(hide);
+
+  $(this).after(new_table);
+
+  var lines_deleted = 0;
+  var lines_added = 0;
+  var line_rows = [];
+  copy.find('tr').each(function () {
+    var lineNumbers = $(this).find('.line_numbers').clone();
+    if ($(this).find('.gd').length == 1) {
+      lines_deleted++;
+
+      var line_row = $('<tr></tr>');
+      line_rows.push(line_row);
+      $(line_row).append(lineNumbers[0]).
+                  append($(this).find('.diff-line').addClass('side-left').each(toEnsp)).
+                  append('<td class="empty-line side-right diff-line line">&nbsp;</td>').
+                  append(lineNumbers[1]);
+    } else if ($(this).find('.gi').length == 1) {
+      lines_added++;
+
+      var line_row;
+      if (lines_deleted > 0) {
+        line_row = line_rows[line_rows.length - lines_deleted];
+        lines_deleted--;
+      } else {
+        line_row = $('<tr><td></td><td></td><td></td><td></td></tr>');
+        line_rows.push(line_row);
+
+        $(':nth-child(1)', line_row).replaceWith(lineNumbers[0]); // empty line number
+        $(':nth-child(2)', line_row).replaceWith('<td class="empty-line side-left diff-line line">&nbsp;</td>');
+      }
+      $(':nth-child(3)', line_row).replaceWith($(this).find('.diff-line').addClass('side-right').each(toEnsp));
+      $(':nth-child(4)', line_row).replaceWith(lineNumbers[1]);
+    } else {
+      // reset
+      lines_added = 0;
+      lines_deleted = 0;
+
+      var line_row = $('<tr></tr>');
+      $(line_row).append(lineNumbers[0]).
+                  append($(this).find('.diff-line').clone().addClass('side-left').each(toEnsp)).
+                  append($(this).find('.diff-line').clone().addClass('side-right').each(toEnsp)).
+                  append(lineNumbers[1]);
+      line_rows.push(line_row);
+    }
+  });
+
+  for (var i in line_rows) {
+    $(new_table).append($(line_rows[i]));
+  }
+
+});
+
+$(".disabled-diff-table").each(function() {
+  var left = $(this).clone();
+  var right = $(this).clone();
   $(this).after('<div class="side-by-side-diff" style="display:none"><div class="side-left"></div><div class="side-right"></div></div>');
   $(this).siblings().find('.side-left').append(left);
   $(this).siblings().find('.side-right').append(right);
@@ -62,7 +123,7 @@ $(".diff-table").each(function() {
       }
     } else {
       // reset
-      removed = 0;
+      lines_removed = 0;
     }
   });
 
@@ -86,7 +147,7 @@ $(".diff-table").each(function() {
       }
     } else {
       // reset
-      removed = 0;
+      lines_added = 0;
     }
   });
 });
